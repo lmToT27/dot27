29257775202598ad94c6ebd98d11bfe590a35145
lmToT27 <168084827+lmToT27@users.noreply.github.com>
2026-08-25 17:50:10 +0700

    perf: optimize cava script

--- dotfiles/scripts/cava.sh
+++ dotfiles/scripts/cava.sh
@@ -4,12 +4,17 @@ cava_config="$HOME/.config/cava/config_waybar"
 
 echo "⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀⣀"
 
-stdbuf -oL cava -p "$cava_config" | awk -F';' 'BEGIN {
+cava -p "$cava_config" 2>/dev/null | awk -F';' '
+BEGIN {
     split("⣀ ⣄ ⣤ ⣦ ⣶ ⣷ ⣾ ⣿", dict, " ")
 }
 {
+    # 3. BỘ LỌC TỐI THƯỢNG: Chỉ xử lý nếu dòng có đúng 13 trường dữ liệu
+    # (12 giá trị sóng + 1 khoảng trống phía sau dấu chấm phẩy cuối cùng)
+    if (NF != 13) next
+    
     out = ""
-    for (i = 1; i < NF; i++) {
+    for (i = 1; i <= 12; i++) {
         out = out dict[$i + 1]
     }
     print out
